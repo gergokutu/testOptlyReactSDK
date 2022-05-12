808f3971fc6245ef227fe4d1e4abeca6dba73e80
gergokutu <gergo.kutu@gmail.com>
2022-05-12 17:14:00 +0200

    Clean codes - OptimizelyConfig log messages

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import App from './App';
-import { OptimizelyProvider, createInstance, setLogLevel } from '@optimizely/react-sdk';
+import { OptimizelyProvider, createInstance, setLogLevel, enums } from '@optimizely/react-sdk';
 
 // Instantiate an Optimizely client
 const optimizelyClient = createInstance({
@@ -16,100 +16,100 @@ const optimizelyClient = createInstance({
 setLogLevel('debug');
 
 // Read the OptimizelyConfig object
-console.log('***** Optimizely *****');
+setTimeout(() => {
+    const config = optimizelyClient.getOptimizelyConfig();
 
-// setTimeout(() => {
-//     const config = optimizelyClient.getOptimizelyConfig();
+    console.log('***** OptimizelyConfig START *****');
 
-//     console.log(`[OptimizelyConfig] revision = ${config.revision}`);
-//     console.log(`[OptimizelyConfig] sdkKey = ${config.sdkKey}`);
-//     console.log(`[OptimizelyConfig] environmentKey = ${config.environmentKey}`);
+    console.log(`[OptimizelyConfig] -- revision = ${config.revision}`);
+    console.log(`[OptimizelyConfig] -- sdkKey = ${config.sdkKey}`);
+    console.log(`[OptimizelyConfig] -- environmentKey = ${config.environmentKey}`);
 
-//     console.log(`[OptimizelyConfig] attributes:`);
-//     config.attributes.forEach((attribute) => {
-//     console.log(`[OptimizelyAttribute]   -- (id, key) = (${attribute.id}, ${attribute.key})`);
-//     });
+    console.log(`[OptimizelyConfig] attributes:`);
+    config.attributes.forEach((attribute) => {
+        console.log(`[OptimizelyAttribute] -- (id, key) = (${attribute.id}, ${attribute.key})`);
+    });
 
-//     console.log(`[OptimizelyConfig] audiences:`);
-//     config.audiences.forEach((audience) => {
-//     console.log(
-//         `[OptimizelyAudience]   -- (id, name, conditions) = = (${audience.id}, ${audience.name}, ${audience.conditions})`
-//     );
-//     });
+    console.log(`[OptimizelyConfig] audiences:`);
+    config.audiences.forEach((audience) => {
+        console.log(
+            `[OptimizelyAudience] -- (id, name, conditions) = (${audience.id}, ${audience.name}, ${audience.conditions})`
+        );
+    });
 
-//     console.log(`[OptimizelyConfig] events:`);
-//     config.events.forEach((event) => {
-//     console.log(
-//         `[OptimizelyEvent]   -- (id, key, experimentIds) = (${event.id}, ${event.key}, ${event.experimentsIds})`
-//     );
-//     });
+    console.log(`[OptimizelyConfig] events:`);
+        config.events.forEach((event) => {
+        console.log(
+            `[OptimizelyEvent] -- (id, key, experimentIds) = (${event.id}, ${event.key}, ${event.experimentsIds})`
+        );
+    });
 
-//     // all flags
-//     const flags = [];
-//     const flagKeys = [];
-//     for (var key in config.featuresMap) {
-//     flags.push(config.featuresMap[key]);
-//     flagKeys.push(key);
-//     }
+    // all flags
+    const flags = [];
+    const flagKeys = [];
+    for (var key in config.featuresMap) {
+        flags.push(config.featuresMap[key]);
+        flagKeys.push(key);
+    }
 
-//     flagKeys.forEach((flagKey) => {
-//     const flag = config.featuresMap[flagKey];
+    flagKeys.forEach((flagKey) => {
+        const flag = config.featuresMap[flagKey];
 
-//     const experimentRules = flag.experimentRules;
-//     const deliveryRules = flag.deliveryRules;
+        const experimentRules = flag.experimentRules;
+        const deliveryRules = flag.deliveryRules;
 
-//     // use experiment rules and delivery rules and other flag data here...
+        // use experiment rules and delivery rules and other flag data here...
+        experimentRules.forEach((experiment) => {
+            console.log(`[OptimizelyExperiment] -- experiment rule-key = ${experiment.key}`);
+            console.log(`[OptimizelyExperiment] -- experiment audiences = ${experiment.audiences}`);
 
-//     experimentRules.forEach((experiment) => {
-//         console.log(`[OptimizelyExperiment]   - experiment rule-key = ${experiment.key}`);
-//         console.log(`[OptimizelyExperiment]   - experiment audiences = ${experiment.audiences}`);
+            const variationsMap = experiment.variationsMap;
+            const variationKeys = [];
 
-//         const variationsMap = experiment.variationsMap;
-//         const variationKeys = [];
+            for (var key in variationsMap) {
+                variationKeys.push(key);
+            }
 
-//         for (var key in variationsMap) {
-//         variationKeys.push(key);
-//         }
+            variationKeys.forEach((varKey) => {
+                const variation = variationsMap[varKey];
+                console.log(
+                    `[OptimizelyVariation] -- variation = { key: ${varKey}, id: ${variation.id}, featureEnabled: ${variation.featureEnabled} })`
+                );
 
-//         variationKeys.forEach((varKey) => {
-//         const variation = variationsMap[varKey];
-//         console.log(
-//             `[OptimizelyVariation]       -- variation = { key: ${varKey}, id: ${variation.id}, featureEnabled: ${variation.featureEnabled} })`
-//         );
+                const variablesMap = variationsMap[varKey].variablesMap;
+                const variableKeys = [];
 
-//         const variablesMap = variationsMap[varKey].variablesMap;
-//         const variableKeys = [];
+                for (var key in variablesMap) {
+                    variableKeys.push(key);
+                }
 
-//         for (var key in variablesMap) {
-//             variableKeys.push(key);
-//         }
+                variableKeys.forEach((variableKey) => {
+                    const variable = variablesMap[variableKey];
+                    console.log(`[OptimizelyVariable] -- variable: ${variableKey}, ${variable.value}`);
+                });
+            });
+        });
 
-//         variableKeys.forEach((variableKey) => {
-//             const variable = variablesMap[variableKey];
-//             console.log(`[OptimizelyVariable]           --- variable: ${variableKey}, ${variable}`);
-//         });
-//         });
-//     });
+        deliveryRules.forEach((delivery) => {
+            console.log(`[OptimizelyExperiment] -- delivery rule-key = ${delivery.key}`);
+            console.log(`[OptimizelyExperiment] -- delivery audiences = ${delivery.audiences}`);
 
-//     deliveryRules.forEach((delivery) => {
-//         console.log(`[OptimizelyExperiment]   - delivery rule-key = ${delivery.key}`);
-//         console.log(`[OptimizelyExperiment]   - delivery audiences = ${delivery.audiences}`);
+            // use delivery rule data here...
+        });
+    });
 
-//         // use delivery rule data here...
-//     });
-//     });
+    // listen to OPTIMIZELY_CONFIG_UPDATE to get updated data
 
-//     // listen to OPTIMIZELY_CONFIG_UPDATE to get updated data
-
-//     optimizelyClient.notificationCenter.addNotificationListener(
-//     enums.NOTIFICATION_TYPES.OPTIMIZELY_CONFIG_UPDATE,
-//     function () {
-//         const newConfig = optimizelyClient.getOptimizelyConfig();
-//         console.log(`[OptimizelyConfig] revision = ${newConfig.revision}`);
-//     }
-//     );
-//     // End OptimizelyConfig
-// }, 1000)
+    optimizelyClient.notificationCenter.addNotificationListener(
+        enums.NOTIFICATION_TYPES.OPTIMIZELY_CONFIG_UPDATE,
+        function () {
+            const newConfig = optimizelyClient.getOptimizelyConfig();
+            console.log(`[OptimizelyConfig] -- revision = ${newConfig.revision}`);
+        }
+    );
+    // End OptimizelyConfig
+    console.log('***** OptimizelyConfig END *****');
+}, 1000)
 
 class AppWrapper extends Component {
     render() {
