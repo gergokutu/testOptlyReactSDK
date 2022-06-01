05e2c24ccd07cf63bd27462986dfa803c309a820
gergokutu <gergo.kutu@gmail.com>
2022-06-01 09:51:50 +0200

    Display that the info coming via OptimizelyExperiment

--- src/components/VipContent/index.js
+++ src/components/VipContent/index.js
@@ -9,25 +9,30 @@ static contextType = OptimizelyContext;
             <OptimizelyExperiment experiment='workramp_new_node_experiment'>
             { (variation) => (
                 variation
-                ?
-                <div>
-                    <div>User is bucketed into "{ variation }" to play: { this.props.optimizely.getFeatureVariableString('workramp_new_node', 'game').toUpperCase() }</div>
-                    <div>User Id: { this.context.optimizely.user.id }</div>
-                    <div>VIP member: { this.context.optimizely.user.attributes.isVIP ? 'YES' : 'NO'}</div>
-                    <div>Location: { this.context.optimizely.user.attributes.location }</div>
-                    <div>Location: { this.context.optimizely.user.attributes.age }</div>
-                    <div>isMab: { this.context.optimizely.user.attributes.mab ? 'True' : 'False' }</div>
-                    { this.props.optimizely.track('testEvent_01') }
-                </div> 
+                ?   (
+                        <div>
+                            <div>***** Via OptimizelyExperiment *****</div>
+                            <div>User is bucketed into "{ variation }" to play: { this.props.optimizely.getFeatureVariableString('workramp_new_node', 'game').toUpperCase() }</div>
+                            <div>User Id: { this.context.optimizely.user.id }</div>
+                            <div>VIP member: { this.context.optimizely.user.attributes.isVIP ? 'YES' : 'NO'}</div>
+                            <div>Location: { this.context.optimizely.user.attributes.location }</div>
+                            <div>Location: { this.context.optimizely.user.attributes.age }</div>
+                            <div>isMab: { this.context.optimizely.user.attributes.mab ? 'True' : 'False' }</div>
+                            { this.props.optimizely.track('testEvent_01') }
+                        </div> 
+                    )    
                 :
-                <div>
-                    <div>You will need VIP membership to enter Experiment 1! But still able to play: { this.props.optimizely.getFeatureVariableString('workramp_new_node', 'game').toUpperCase() }</div>
-                    <div>User Id: { this.context.optimizely.user.id }</div>
-                    <div>VIP member: { this.context.optimizely.user.attributes.isVIP ? 'YES' : 'NO'}</div>
-                    <div>Location: { this.context.optimizely.user.attributes.location }</div>
-                    <div>Location: { this.context.optimizely.user.attributes.age }</div>
-                    <div>isMab: { this.context.optimizely.user.attributes.mab ? 'True' : 'False' }</div>
-                </div> 
+                    (
+                        <div>
+                            <div>***** Via OptimizelyExperiment *****</div>
+                            <div>You will need VIP membership to enter Experiment 1! But still able to play: { this.props.optimizely.getFeatureVariableString('workramp_new_node', 'game').toUpperCase() }</div>
+                            <div>User Id: { this.context.optimizely.user.id }</div>
+                            <div>VIP member: { this.context.optimizely.user.attributes.isVIP ? 'YES' : 'NO'}</div>
+                            <div>Location: { this.context.optimizely.user.attributes.location }</div>
+                            <div>Location: { this.context.optimizely.user.attributes.age }</div>
+                            <div>isMab: { this.context.optimizely.user.attributes.mab ? 'True' : 'False' }</div>
+                        </div> 
+                    )
             )}
             </OptimizelyExperiment>
         );
